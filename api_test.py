cf0a87c8712b506e88f193ae3b320439d643a51e
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2026-02-11 23:54:28 +0200

    perfect test

---
+++ api_test.py
@@ -0,0 +1,20 @@
+﻿import pytest
+import requests
+from config import BASE_URL, ENDPOINTS, TEST_USERS
+
+@pytest.mark.parametrize('user', TEST_USERS)
+def test_create_post(user):
+    url = BASE_URL + ENDPOINTS['posts']
+    response = requests.post(url, json=user)
+    assert response.status_code == 201
+    data = response.json()
+    assert data['title'] == user['title']
+    assert data['body'] == user['body']
+    assert data['userId'] == user['userId']
+
+@pytest.mark.parametrize('endpoint', ['posts', 'comments', 'todos'])
+def test_get_endpoints(endpoint):
+    url = BASE_URL + ENDPOINTS[endpoint]
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
---
+++ config.py
@@ -0,0 +1,12 @@
+﻿BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+ENDPOINTS = {
+    'posts': '/posts',
+    'comments': '/comments',
+    'todos': '/todos'
+}
+
+TEST_USERS = [
+    {'userId': 1, 'title': 'foo', 'body': 'bar'},
+    {'userId': 2, 'title': 'hello', 'body': 'world'}
+]
